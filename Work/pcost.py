e19f30b52adfb1dad9528bd747141b5e645858e9
djclayton21 <clayton.daniel.j@gmail.com>
2020-06-19 13:37:43 -0600

    1.6: files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+path = "Data/portfolio.csv"
+cost = 0
+
+
+with open(path, "rt") as portfolio:
+    next(portfolio)
+    for stock in portfolio:
+        deets = stock.split(",")
+        shares = int(deets[1])
+        price = float(deets[2])
+        cost += shares * price
+
+print(f"Total cost is ${cost:.2f}")
