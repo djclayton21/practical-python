44340d17db3497675f8593a578503979f2e50a87
djclayton21 <clayton.daniel.j@gmail.com>
2020-06-20 12:06:07 -0600

    read portfolio to tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,21 @@
 # report.py
 #
 # Exercise 2.4
+
+
+def read_portfolio(filename):
+    import csv
+
+    portfolio = []
+
+    with open(filename, "rt") as portfolio_file:
+        lines = csv.reader(portfolio_file)
+        header = next(lines)
+        for entries in lines:
+            name = entries[0]
+            nShares = int(entries[1])
+            sharePrice = float(entries[2])
+
+            portfolio.append((name, nShares, sharePrice))
+    return portfolio
+
