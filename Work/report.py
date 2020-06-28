4bd281a6432fa1428f0a365cd005a52d53d2ddd3
djclayton21 <clayton.daniel.j@gmail.com>
2020-06-28 11:21:40 -0600

    switch to parse_csv

--- Work/report.py
+++ Work/report.py
@@ -4,44 +4,7 @@
 
 from pprint import pprint
 import sys
-import csv
-
-
-def read_portfolio(filename):
-
-    portfolio = []
-
-    with open(filename, "rt") as portfolio_file:
-        lines = csv.reader(portfolio_file)
-        header = next(lines)
-        for i, line in enumerate(lines):
-            record = dict(zip(header, line))
-            try:
-                holding = {}
-                holding["symbol"] = record["name"]
-                holding["n_shares"] = int(record["shares"])
-                holding["purchase_price"] = float(record["price"])
-                portfolio.append(holding)
-            except ValueError:
-                print(f"Could not process line {i}:", line)
-
-    return portfolio
-
-
-def read_prices(filename):
-
-    prices = {}
-
-    with open(filename, "rt") as prices_file:
-        lines = csv.reader(prices_file)
-
-        for price in lines:
-            if len(price) != 2:
-                continue
-
-            prices[price[0]] = float(price[1])
-
-    return prices
+from fileparse import parse_csv
 
 
 def update_portfolio(portfolio, prices):
@@ -49,31 +12,31 @@ def update_portfolio(portfolio, prices):
     result = {"holdings": [], "total_value": 0, "total_p/l": 0}
 
     for stock in portfolio:
-        stock["current_price"] = prices[stock["symbol"]]
-        stock["change"] = stock["current_price"] - stock["purchase_price"]
-        stock["value"] = stock["n_shares"] * stock["current_price"]
+        stock["current_price"] = prices[stock["name"]]
+        stock["change"] = stock["current_price"] - stock["price"]
+        stock["value"] = stock["shares"] * stock["current_price"]
         stock["value"] = round(stock["value"], 2)
         stock["change"] = round(stock["change"], 2)
 
         result["holdings"].append(stock)
         result["total_value"] += stock["value"]
-        result["total_p/l"] += stock["n_shares"] * stock["change"]
+        result["total_p/l"] += stock["shares"] * stock["change"]
 
     return result
 
 
 def make_report(portfolio):
     """
-    print a report from a portfolio including keys symbol, n_shares,
+    print a report from a portfolio including keys name, shares,
     current_price, and change
     """
-    header = "%10s %10s %10s %10s" % ("Symbol", "Shares", "Price", "Change")
+    header = "%10s %10s %10s %10s" % ("name", "Shares", "Price", "Change")
     print(header)
     print(len(header) * "-")
     for stock in portfolio["holdings"]:
         line = "%10s %10d %10s %10.2f" % (
-            stock["symbol"],
-            stock["n_shares"],
+            stock["name"],
+            stock["shares"],
             f'${stock["current_price"]:0.2f}',
             stock["change"],
         )
@@ -85,8 +48,12 @@ def portfolio_report(
 ):
     "print a report on your portfolio from current prices"
 
-    portfolio = read_portfolio(portfolio_filename)
-    current_prices = read_prices(prices_filename)
+    portfolio = parse_csv(
+        portfolio_filename, select=["name", "shares", "price"], types=[str, int, float]
+    )
+    current_prices = dict(
+        parse_csv(prices_filename, has_headers=False, types=[str, float])
+    )
     current_portfolio = update_portfolio(portfolio, current_prices)
     make_report(current_portfolio)
 
